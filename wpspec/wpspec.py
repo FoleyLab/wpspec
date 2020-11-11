e7a0446bb9e4bc65a285669c3e53e0a4456ff8c9
FoleyLab <jayfoleyiv@gmail.com>
2020-11-11 13:42:03 -0500

    added pir class

--- wpspec/wpspec.py
+++ wpspec/wpspec.py
@@ -43,12 +43,15 @@ class pib:
         else:
             self.L = 1
         if 'particle_mass' in args:
-            self.m = args['particle_mass']
+            self.M = args['particle_mass']
         else:
-            self.m =  1
-            
+            self.M =  1
+        
+        """ array of x-values """
         self.x = np.linspace(0, self.L, self.grid_points)
-
+        
+        """ reduced plancks constant in atomic units """
+        self.hbar = 1
 
     def eigenfunction(self):
         """ 
@@ -81,9 +84,74 @@ class pib:
         self.E : float
             Energy eigenvalue of the PIB 
         """
-        self.E = self.n ** 2 * np.pi **2 / (2 * self.m * self.L **2) 
+        self.E = self.n ** 2 * np.pi **2 * self.hbar ** 2  / (2 * self.M * self.L **2) 
         
 
+class pir:
+    def __init__(self, args):
+        if 'grid_points' in args:
+            self.grid_points = args['grid_points']
+        else:
+            self.grid_points = 100
+        if 'quantum_state' in args:
+            self.m = args['quantum_state']
+        else:
+            self.m = 1
+
+        if 'particle_mass' in args:
+            self.M = args['particle_mass']
+        else:
+            self.M =  1
+        if 'bond_length' in args:
+            self.r = args['bond_length']
+        else:
+            self.r = 1.
+        
+        """ array of angles """    
+        self.theta = np.linspace(0, np.pi, self.grid_points)
+        
+        """ moment of inertia """
+        self.I = self.M * self.r **2
+        
+        """ reduced planck's constant in atomic units """
+        self.hbar = 1
+
+    def eigenfunction(self):
+        """ 
+        function to compute the energy eigenfunction of the pib and
+        store to attribute self.psi
+        
+        Parameters
+        ----------
+        self
+        
+        Returns
+        -------
+        self.psi : float array
+            Energy eigenfunction of the PIR 
+        """
+        ci = 0+1j
+        self.psi = np.sqrt( 1. / (2 * np.pi) ) * np.exp(-ci * self.m * self.theta)
+        return 1
+  
+    def eigenvalue(self):
+        """ 
+        function to compute the energy eigenvalue of the pib and
+        store to attribute self.E
+        
+        Parameters
+        ----------
+        self
+        
+        Returns
+        -------
+        self.E : float
+            Energy eigenvalue of the PIB 
+        """
+        self.E = self.m ** 2 * self.hbar **2 /(2 * self.I)
+        return 1
+    
+    
 if __name__ == "__main__":
     # Do something if this file is invoked on its own
     print(canvas())
